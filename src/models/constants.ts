3d73d09eac78a15f678a3f7c2c617c69c9d054bb
Paul Benchea <paul.benchea@optima.ro>
2026-03-13 00:34:23 +0200

    [models] Add ARCHIVE_DIR constant

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -7,6 +7,7 @@ export const APP_VERSION = packageJson.version;
 
 export const WORKING_DIR = path.join(process.cwd(), '.subconductor');
 export const CHECKLISTS_DIR = path.join(WORKING_DIR, 'checklists');
+export const ARCHIVE_DIR = path.join(WORKING_DIR, 'archive/checklists');
 export const CHECKLISTS_INDEX_FILE = path.join(WORKING_DIR, 'checklists.md');
 export const TASK_FILE = path.join(WORKING_DIR, 'tasks.md');
 
